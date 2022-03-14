50c3900d14864d406b7fbff25482b51a5aeb8803
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-03-14 17:12:37 +0530

    Update LockedMe.java

--- LockedMePhase1Project/src/LockedMe.java
+++ LockedMePhase1Project/src/LockedMe.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 public class LockedMe {
 
     static final String projectFilesPath="D:\\Files";
@@ -22,6 +24,29 @@ public class LockedMe {
         }
 	}
     public static void addNewFile() {
+        Scanner obj = new Scanner(System.in);
+            //get file name from user
+            String filename;
+            System.out.println("Enter File name to added: ");
+            filename= obj.nextLine();
+
+            //get no. of lines want to write in file
+            int linesCount;
+            System.out.println("Enter no. of line want to write: ");
+            linesCount = Integer.parseInt(obj.nextLine());
+
+           try{ FileWriter fw = new FileWriter(projectFilesPath+"\\"+filename);
+
+            //read line by line from user
+            for (int i=1; i<=linesCount; i++){
+                System.out.println("Enter line to be write: ");
+                fw.write(obj.nextLine()+"\n");
+            }
+            System.out.println("File Added Successfully");
+            fw.close();
+        } catch (Exception Ex) {
+           System.out.println("Please contact developer");
+        }
 		
 	}
     public static void deleteFile() {
