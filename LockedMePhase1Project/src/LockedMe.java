73a4dbeb859e8cd33c06bb0e4bc59187be91bc2a
roshan-mahale <99242453+roshan-mahale@users.noreply.github.com>
2022-03-14 19:16:12 +0530

    Update LockedMe.java

--- LockedMePhase1Project/src/LockedMe.java
+++ LockedMePhase1Project/src/LockedMe.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class LockedMe {
 
@@ -35,7 +36,7 @@ public class LockedMe {
             System.out.println("Enter no. of line want to write: ");
             linesCount = Integer.parseInt(obj.nextLine());
 
-           try{ FileWriter fw = new FileWriter(projectFilesPath+"\\"+filename);
+           try{ FileWriter fw = new FileWriter(projectFilesPath+"\\"+filename+".txt");
 
             //read line by line from user
             for (int i=1; i<=linesCount; i++){
@@ -50,23 +51,58 @@ public class LockedMe {
 		
 	}
     public static void deleteFile() {
+        try{
         Scanner obj = new Scanner(System.in);
         String fileName;
         System.out.println("Enter file name to deleted: ");
         fileName =obj.nextLine();
+        //case sensitive
+        ArrayList<String> allFilesNames = new ArrayList<String>();
+        File file = new File(projectFilesPath);
+        String [] fileList = file.list();
+        for (var allfiles:fileList){
+            allFilesNames.add(allfiles);
+        }
         File f = new File(projectFilesPath+"\\"+fileName);
         
-        if ( f.exists()){
+        if (allFilesNames.contains(fileName)){
             f.delete();
             System.out.println("File delete Successfully");
         } else{
             System.out.println("File does not found");
         }
-        
+    } catch(Exception Ex) {
+        System.out.println("Unable to delete file. Please contact developer.");
+
+    }
 
 		
 	}
     public static void SearchFiles() {
+        try{
+            Scanner obj = new Scanner(System.in);
+            String fileName;
+            System.out.println("Enter file name to Searched: ");
+            fileName =obj.nextLine();
+            //case sensitive
+            ArrayList<String> allFilesNames = new ArrayList<String>();
+            File file = new File(projectFilesPath);
+            String [] fileList = file.list();
+            for (var allfiles:fileList){
+                allFilesNames.add(allfiles);
+            }
+            File f = new File(projectFilesPath+"\\"+fileName);
+            
+            if (allFilesNames.contains(fileName)){
+                
+                System.out.println("File found Successfully");
+            } else{
+                System.out.println("File does not found");
+            }
+        } catch(Exception Ex) {
+            System.out.println("Unable to delete file. Please contact developer.");
+    
+        }
 		
 	}
 	
